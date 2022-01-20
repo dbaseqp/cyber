a5382eec2d396f08d7857b385ee72a7b1de9e545
Gabriel Fok <gabrielfok128@gmail.com>
2022-01-19 23:08:57 -0800

    Update install.sh

--- term-alert/install.sh
+++ term-alert/install.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-
 YUM_CMD=$(which yum)
 APT_GET_CMD=$(which apt-get)
-
+SYSTEMCTL_EXIST=$(which systemctl)
 if [[ ! -z $YUM_CMD ]]; then
-    yum install epel-release python-urwid python3 python3-pip git -y
+    yum install epel-release python-urwid python3 python3-pip git auditd  -y
 elif [[ ! -z $APT_GET_CMD ]]; then
     apt-get update -y 
-    apt-get install python3 python3-pip git python-urwid -y
+    apt-get install python3 python3-pip git python-urwid auditd  -y
 else
     echo "installation failed"
     exit 1;
 fi
-
 pip3 install urwid
-git clone https://github.com/dbaseqp/term-alert
+git clone https://github.com/dbaseqp/cyber
 cd term-alert
+if [[ ! -z $SYSTEMCTL_EXIST ]]; then
+    systemctl mask systemd-journald-audit.socket
+    systemctl start auditd
+    systemctl stop auditd
+else
+    service auditd start
+fi
+service auditd stop
+chmod +x go-audit
+./go-audit -config go-audit.yaml &
